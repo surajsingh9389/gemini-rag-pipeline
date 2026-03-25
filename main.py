20192bc239060b9e03dfe453aff6471e2f4da327
surajsingh9389 <sk9389623488@gmail.com>
2026-03-25 22:51:09 +0530

    FAISS added of fast nearest neighbor search

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import numpy as np
-import heapq
 from google import genai
 import os
-import pickle
 from dotenv import load_dotenv
+import faiss
 
 
 # 1. Load your .env file
@@ -13,6 +12,7 @@ load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 
+# convert text to embedding 
 def get_embedding(text):
     
     result = client.models.embed_content(
@@ -20,38 +20,16 @@ def get_embedding(text):
     contents=text
     ) 
 
-    return np.array(result.embeddings[0].values)
+    return np.array(result.embeddings[0].values).astype('float32')
 
 
-def get_top_k(scores, k):
-    if k == 0 or k > len(scores): 
-        return []
-    
-    new_scores = heapq.nlargest(k, enumerate(scores), key=lambda x: x[1])
-    
-    return new_scores 
-
+# normalize the vector 
 def safe_normalize(v):
     magnitude = np.linalg.norm(v)
     if magnitude == 0:
         return v
     return v/magnitude
     
-   
-
-def semantic_search(query, normalize_embeddings, sentences, k):
-    
-    embedding_query = get_embedding(query)
-    normalize_query = safe_normalize(embedding_query)
-    
-    # Similarity 
-    scores = [np.dot(normalize_query, v) for v in normalize_embeddings]
-
-    top_matches = get_top_k(scores, k)
-    
-    return [(sentences[i], score) for i, score in top_matches]
-
-
 
 sentences = [
  "I love machine learning",
@@ -60,31 +38,35 @@ sentences = [
  "Football is a great sport"
 ]
 
-CACHE_FILE = r"projects\semantic_search\embeddings.pkl"
+# Sentences embedding
+raw_embeddings = [get_embedding(v) for v in sentences]
 
-# API Call Cost Optimization (Caching)
+# Sentences Normalization 
+normalized_embeddings = [safe_normalize(v) for v in raw_embeddings]
 
-if os.path.exists(CACHE_FILE):
-    print("Loading embeddings from cache...")
-    with open(CACHE_FILE, "rb") as f:
-        normalized_embeddings = pickle.load(f)
-        
-else: 
-    print("Fetching embeddings from Gemini...")
-    raw_embeddings = [get_embedding(v) for v in sentences]
-    # Avoid Normalizing Every Query (Pre-normalization)
-    normalized_embeddings = [safe_normalize(v) for v in raw_embeddings]
-    
-    # This will create the folders if they don't exist
-    os.makedirs(os.path.dirname(CACHE_FILE), exist_ok=True)
-    
-    with open(CACHE_FILE, "wb") as f:
-        pickle.dump(normalized_embeddings, f)
+# for extreme speed, float32 type slightly less precision(compare to 64), but uses half the memory. Each number takes 4 bytes.
+embeddings_matrix = np.array(normalized_embeddings).astype('float32')
+
+# dimension of embedding
+d = embeddings_matrix.shape[1]  # .shape -> (row, cols)
+
+# container or database that holds all your vectors 
+index = faiss.IndexFlatIP(d) # Inner Product (best for cosine)
+
+# Add embeddigs to the database 
+index.add(embeddings_matrix)
 
 query = input("Enter your search query: ")
 k = int(input("How many top matches you want?: "))
-        
-results = semantic_search(query, normalized_embeddings, sentences, k)
 
-print(results)
+query_embedding = get_embedding(query)
+print(query_embedding)
 
+normalized_query = safe_normalize(query_embedding)
+
+normalized_query = np.array([normalized_query]).astype('float32')
+
+distances, indices = index.search(normalized_query, k)
+
+for i, idx in enumerate(indices[0]):
+    print(f"{i+1}. {sentences[idx]} (score: {distances[0][i]:.4f})")
